082bc8a8107191fdefc78eba22c795157f9cb705
otaviosbms <105327547+otaviosbms@users.noreply.github.com>
2022-07-01 21:36:06 -0300

    Add files via upload

---
+++ teste pyton.py
@@ -0,0 +1,11 @@
+a= int(input('primeiro numero'))
+b= int(input('segundo numero'))
+c= input('qual conta vc irá fazer?')
+if c == '+':
+    print(a+b)
+if c == '-':
+    print(a-b)
+if c == '/':
+    print(a/b)
+if c == '*':
+    print(a*b)
