9f125fb9024e261ee4a93f60ef7fe3c3fb90a2a0
Otávio SBM dos Santos <105327547+otaviosbms@users.noreply.github.com>
2022-07-13 20:13:24 -0300

    Update pedrapapeltesoura.py

--- pedrapapeltesoura.py
+++ pedrapapeltesoura.py
@@ -1,22 +1,29 @@
 import random
-pedra = range(1, 3)
-papel = range(4, 6)
-tesoura = range(7, 9)
+pedra = 1
+papel = 2
+tesoura = 3
 
 p1 = input('pedra, papel ou tesoura?')
 
 if p1 == 'pedra':
     print('voce escolheu pedra')
 
+if p1 == 'papel':
+    print('voce escolheu papel')
 
-escolhapc = random.randrange(1, 9)
-print(escolhapc)
-if escolhapc == 1 and escolhapc == 2 and escolhapc = 3:
+if p1 == 'tesoura':
+    print('voce escolheu tesoura')
+
+
+escolhapc = random.randrange(1, 3)
+
+if escolhapc == 1:
     print('o computador escolheu pedra')
     
-if escolhapc == 4 and escolhapc == 5 and escolhapc = 6:
+if escolhapc == 2:
     print('o computador escolheu papel')
     
-if escolhapc == 7 and escolhapc == 8 and escolhapc = 9:
+if escolhapc == 3:
     print('o computador escolheu tesoura')
+
 
