c86ec2bbf1995b36aafd1e73fe9a99bba3ecbdb7
Otávio SBM dos Santos <105327547+otaviosbms@users.noreply.github.com>
2022-07-14 23:39:18 -0300

    Update exercicio  tabuada.py

--- exercicio  tabuada.py
+++ exercicio  tabuada.py
@@ -2,3 +2,11 @@ a = int(input('Digite o número: \n'))
 
 for x in range(11):
     print(a*x)
+    
+   
+#segunda opção:
+    
+#a = int(input('Digite o número: \n'))
+
+#for x in range(1,11):
+    #print(f'{a} X {x} =',a*x)
