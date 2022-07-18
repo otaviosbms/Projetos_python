e3de1b684295c4cf20e67979e991f36c0bb5b1b5
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2022-07-18 00:24:35 -0300

    Update pedrapapeltesoura.py

--- pedrapapeltesoura.py
+++ pedrapapeltesoura.py
@@ -1,55 +1,52 @@
 #decisão player
 
 print('____PEDRA PAPEL OU TESOURA PYTHON____')
+print('\n...................')
 
 p1 = input('\nEscolha: pedra, papel ou tesoura?\n')
 
 if p1 == 'pedra':
-    print('voce escolheu', p1)
+    print('voce escolheu:', p1)
 
 if p1 == 'papel':
-    print('voce escolheu', p1)
+    print('voce escolheu:', p1)
 
 if p1 == 'tesoura':
-    print('voce escolheu', p1)
+    print('voce escolheu:', p1)
 
 while p1 != 'pedra' and p1 != 'papel' and p1 != 'tesoura' and p1 != 'Pedra' and p1 != 'Papel' and p1 != 'Tesoura':
     p1 = str(input("A sua escolha é inválida.\nEscolha outra opção:\n"))
     if p1 == 'pedra' or 'Pedra':
-        print('voce escolheu', p1)
+        print('\nvoce escolheu:', p1)
         break
     if p1 == 'papel' or 'Papel':
-        print('voce escolheu', p1)
+        print('\nvoce escolheu:', p1)
         break
     if p1 == 'tesoura' or 'Tesoura':
-        print('voce escolheu', p1)
+        print('\nvoce escolheu:', p1)
         break
+print('\n...................')
 
 #decisão pc
 
-
 import random
 
-    
 escolhapc = random.randrange(1, 4)
 
-
-
 if escolhapc == 1:
-    print('\nO computador escolheu pedra')
+    print('\nO computador escolheu: pedra')
     
 if escolhapc == 2:
-    print('\nO computador escolheu papel')
+    print('\nO computador escolheu: papel')
     
 if escolhapc == 3:
-    print('\nO computador escolheu tesoura')
+    print('\nO computador escolheu: tesoura')
 
+print('\n...................')
 
 #resultados:
 
 r = (p1, escolhapc)
-
-
 
 while True:
     if r == ('pedra', 1) or r == ('Pedra', 1):
@@ -79,4 +76,5 @@ while True:
     if r == ('pedra', 3) or r == ('Pedra', 3):
         print('\nVoce ganhou!')
         break
+
 
