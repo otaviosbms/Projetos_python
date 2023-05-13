8ef26f4278feae179d9b723f8a3d18d34bb33217
otaviosbms <jaspionj35@gmail.com>
2023-05-13 14:06:34 -0300

    arquivos duplicados deletados

--- exercicio fatorial com for.py
+++
@@ -1,7 +0,0 @@
-def fatorial(numero):
-  resultado = 1
-  for n in range(1,numero+1):
-    resultado = resultado * n
-    print(resultado)
-
-fatorial(5)
--- exercicio minha função.py
+++
@@ -1,3 +0,0 @@
-def media(x, y):
-    print((x + y) / 2)
-media(5, 5)
